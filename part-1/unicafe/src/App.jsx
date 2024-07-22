02661de7592a36f06557813737b8a1c8542fd866
iamadams <iamadams4523@gmail.com>
2024-07-22 13:58:21 +0100

    refactored statistics

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from 'react';
 
+const Statistics = ({ good, bad, neutral }) => {
+  return (
+    <>
+      <h2>Statistics</h2>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good + neutral + bad) / 3}</p>
+      <p>positive {(good + neutral) / 3}</p>
+    </>
+  );
+};
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -29,16 +43,7 @@ const App = () => {
         <button onClick={handleNeutral}>neutral</button>
         <button onClick={handleBad}>bad</button>
       </div>
-      <div>
-        <h2>Statistics</h2>
-
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good + neutral + bad}</p>
-        <p>average {(good + neutral + bad) / 3}</p>
-        <p>positive {(good + neutral) / 3}</p>
-      </div>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </>
   );
 };
