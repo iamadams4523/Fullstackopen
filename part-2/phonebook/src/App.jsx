6480b06d7b5578c7f534882ffd93cb99bfec0241
iamadams <iamadams4523@gmail.com>
2024-08-01 09:11:27 +0100

    exercise 2.13 done

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,31 +1,41 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from './components/Filter';
 import Person from './components/Person';
 import PersonForm from './components/PersonForm';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNum, setNewNum] = useState('');
   const [filter, setFilter] = useState('');
   const [showFilter, setShowFilter] = useState(false);
+
+  useEffect(() => {
+    console.log('effect');
+    axios.get('http://localhost:3001/persons').then((response) => {
+      console.log('promise fulfilled');
+      setPersons(response.data);
+    });
+  }, []);
 
   const addName = (e) => {
     e.preventDefault();
     const nameObject = {
       name: newName,
       number: newNum,
+      id: persons.length + 1,
     };
 
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat(nameObject));
+      axios
+        .post('http://localhost:3001/persons', nameObject)
+        .then((response) => {
+          console.log('Request sent', response);
+          setPersons(persons.concat(nameObject));
+        });
     }
     setNewName('');
     setNewNum('');
